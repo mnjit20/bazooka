4751bfff15af0dd29037199e48dd1a386ac15e98
mnjit20 <mnjit.vrma@gmail.com>
2018-07-11 12:58:52 +0530

    initial

---
+++ src/components/layout/leftnavbar.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class LeftNavBar extends Component {
+  render() {
+    return (
+      <div className="leftBox">
+        <ul>
+          <li className="submenu active">
+            <a className="dashboard" href="/dashboard">Dashboard</a>
+          </li>
+          <li className="submenu">
+            <a className="project" href="">Projects</a>
+            <ul>
+              <li>
+                <a href="/addProject">Add New</a>
+              </li>
+              <li>
+                <a href="/projectList">Ongoing</a>
+              </li>
+
+              <li>
+                <a href="/completedProjectList">Completed</a>
+              </li>
+
+            </ul>
+          </li>
+          <li className="submenu">
+            <a className="partner" href="">Partner</a>
+            <ul>
+              <li>
+                <a href="/creators-list">Creators</a>
+              </li>
+              <li>
+                <a href="/influencers-list">Influencers</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+    )
+  }
+}
+
+export default LeftNavBar;
---
+++ src/components/layout/topnavbar.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class TopNavbar extends Component {
+  render() {
+    return (
+      <div className="topBox">
+        <div className="leftTop">
+          <span className="btn"></span>
+          <span className="logo">
+            <a href="http://aspire.firstwall.com/dashboard/">
+              <img src="http://aspire.firstwall.com/assets/images/logo.png" />
+            </a>
+          </span>
+        </div>
+
+        <div className="rightTop">
+          <ul>
+            <li>
+              <span className="user">
+                <img src="http://aspire.firstwall.com/assets/images/user.png" />
+              </span>
+            </li>
+            <li>
+              <a className="bell" href="">bell</a>
+            </li>
+            <li>
+              <a className="setting" href="">setting</a>
+            </li>
+            <li>
+              <a target="_self" className="logout" href="http://aspire.firstwall.com/logout/">logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default TopNavbar;
