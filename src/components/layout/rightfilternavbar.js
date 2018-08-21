50fa334665f6ff4a9a3b135acf7ad3c386a2d719
mnjit <mnjit.vrma@gmail.com>
2018-08-21 15:48:55 +0530

    states list with redux added

--- src/components/layout/rightfilternavbar.js
+++ src/components/layout/rightfilternavbar.js
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
 //import { Link } from 'react-router-dom';
 
 class RightFilterNavBar extends Component {
+  componentDidMount() {
+    //    const states = this.props.states;
+    console.log("-----data is", this.props.states);
+  }
   render() {
     return (
       <div className="rightBox ng-scope">
@@ -27,6 +33,7 @@ class RightFilterNavBar extends Component {
                             value="39"
                             id="chkbox_39"
                           />
+
                           <span className="">Animation</span>
                         </label>
                       </li>
@@ -73,70 +80,14 @@ class RightFilterNavBar extends Component {
                 <option label="All" value="string:All">
                   All
                 </option>
-                <option
-                  label="Andaman and Nico.In."
-                  value="string:Andaman and Nico.In.">
-                  Andaman and Nico.In.
-                </option>
-                <option label="Andhra Pradesh" value="string:Andhra Pradesh">
-                  Andhra Pradesh
-                </option>
-                <option label="Assam" value="string:Assam">
-                  Assam
-                </option>
-                <option label="Bihar" value="string:Bihar">
-                  Bihar
-                </option>
-                <option label="Chandigarh" value="string:Chandigarh">
-                  Chandigarh
-                </option>
-                <option label="Chhattisgarh" value="string:Chhattisgarh">
-                  Chhattisgarh
-                </option>
-                <option label="Delhi" value="string:Delhi">
-                  Delhi
-                </option>
-                <option label="Goa" value="string:Goa">
-                  Goa
-                </option>
-                <option label="Gujarat" value="string:Gujarat">
-                  Gujarat
-                </option>
-                <option label="Haryana" value="string:Haryana">
-                  Haryana
-                </option>
-                <option
-                  label="Himachal Pradesh"
-                  value="string:Himachal Pradesh">
-                  Himachal Pradesh
-                </option>
-                <option label="Jharkhand" value="string:Jharkhand">
-                  Jharkhand
-                </option>
-                <option label="Karnataka" value="string:Karnataka">
-                  Karnataka
-                </option>
-                <option label="Madhya Pradesh" value="string:Madhya Pradesh">
-                  Madhya Pradesh
-                </option>
-                <option label="Maharashtra" value="string:Maharashtra">
-                  Maharashtra
-                </option>
-                <option label="Punjab" value="string:Punjab">
-                  Punjab
-                </option>
-                <option label="State" value="string:State">
-                  State
-                </option>
-                <option label="Tamil Nadu" value="string:Tamil Nadu">
-                  Tamil Nadu
-                </option>
-                <option label="Uttarakhand" value="string:Uttarakhand">
-                  Uttarakhand
-                </option>
-                <option label="West Bengal" value="string:West Bengal">
-                  West Bengal
-                </option>
+                {this.props.states.map(stateObj => (
+                  <option
+                    key={stateObj.id}
+                    label={stateObj.name}
+                    value={stateObj.name}>
+                    {stateObj.name}
+                  </option>
+                ))}
               </select>
             </div>
 
@@ -150,4 +101,8 @@ class RightFilterNavBar extends Component {
   }
 }
 
-export default RightFilterNavBar;
+//export default RightFilterNavBar;
+
+const mapStateToProps = state => state;
+const connectedRightFilterNavBar = connect(mapStateToProps)(RightFilterNavBar);
+export default connectedRightFilterNavBar;
